8cfbba8bcb5030f550b8a82a8ab11a876f1f9ca5
Vitor Ricardo <vitorricardo@outlook.com>
2021-11-09 09:03:31 -0300

    feat: first commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# gerar-boletos
+
+#### NPM badges
+<!-- [START badges] -->
+[![NPM gerar-boletos package](https://img.shields.io/npm/v/gerar-boletos.svg)](https://npmjs.org/package/gerar-boletos)
+[![npm downloads](https://img.shields.io/npm/dm/gerar-boletos.svg?maxAge=604800)](https://npm-stat.com/charts.html?package=gerar-boletos&from=2017-01-1)
+[![npm downloads](https://img.shields.io/npm/dt/gerar-boletos.svg?maxAge=604800)](https://npm-stat.com/charts.html?package=gerar-boletos&from=2017-01-1)
+<!-- [END badges] -->
+
+# CRÉDITOS
+
+Este repositório é uma adaptação [deste aqui](https://github.com/Romulosanttos/gerar-boletos). Todo o crédito é dele. 
+
+# Descrição
+
+Biblioteca em Node.js para geração de boletos utilizando PDFKit 
+
+Geração de boletos para bancos:
+- Bradesco
+- Caixa
+- Ailos (Cecred)
+- Itaú
+- Sicoob
+- Sicredi
+- Santander
+- Banco do Brasil
+
+### Install
+
+```javascript
+npm i gerar-boletos
+```
+```javascript
+yarn add gerar-boletos
+```
+### Exemplos de uso
+
+```javascript
+npm run gerarBrasil
+```
+```javascript
+npm run gerarBradesco
+```
+```javascript
+npm run gerarCecred
+```
+```javascript
+npm run gerarItau
+```
+* [pasta com arquivos de exemplo](/examples)
+
+### Run tests
+
+```javascript
+npm run test
+```
+
+
---
+++ examples/gerar-boleto-itau.js
@@ -0,0 +1,60 @@
+const { Bancos, Boletos, streamToPromise } = require('../lib/index');
+
+const boleto = {
+  banco: new Bancos.Itau(),
+  pagador: {
+    nome: 'VITOR RICARDO',
+    endereco: {
+      logradouro: 'R. TANTO FAZ, 15',
+      bairro: 'BAIRRO NAO SEI',
+      cidade: 'SOROCABA',
+      estadoUF: 'SP',
+      cep: '18530-000'
+    }
+  },
+  instrucoes: [''],
+  localPagamento: 'PAGAVEL EM QUALQUER BANCO, OU CORRESPONDENTE NÃO BANCÁRIO, MESMO APÓS O VENCIMENTO',
+  beneficiario: {
+    nome: 'NOME EMPRESA',
+    cnpj: '18795541000119',
+    dadosBancarios: {
+      carteira: '109',
+      agencia: '0000',
+      agenciaDigito: '0',
+      conta: '00000',
+      contaDigito: '0',
+      nossoNumero: '00000000',
+      nossoNumeroDigito: '0'
+    },
+    endereco: {
+      logradouro: 'RUA',
+      bairro: 'BAIRRO',
+      cidade: 'SOROCABA',
+      estadoUF: 'SP',
+      cep: '18030-005'
+    }
+  },
+  boleto: {
+    numeroDocumento: '',
+    especieDocumento: 'DS',
+    valor: 1.00,
+    datas: {
+      vencimento: '2021-11-08',
+      processamento: '2021-11-08',
+      documentos: '2021-11-08'
+    },
+    codigoDeBarras: '00000000000000000000000000000000000000000000',
+    linhaDigitavel: '00000.00000 00000.000000 00000.000000 0 00000000000000'
+  }
+};
+
+const novoBoleto = new Boletos(boleto);
+novoBoleto.gerarBoleto();
+
+novoBoleto.pdfFile().then(async ({ stream }) => {
+  // ctx.res.set('Content-type', 'application/pdf');	
+  await streamToPromise(stream);
+}).catch((error) => {
+  return error;
+});
+
